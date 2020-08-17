9a10e2c8db38376a92b37a56807b411c1a59b451
陳立偉 <chenliwei@weis-MacBook-Pro.local>
2020-08-17 08:28:04 +0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Tim:blacktea99@cluster0.sqeu9.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://Tim:blacktea99@cluster0.sqeu9.gcp.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
